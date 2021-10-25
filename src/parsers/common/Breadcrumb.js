1a729ff9f23f482b8a0e760865cdc4e3828dfd00
Naloaty <53543446+Naloaty@users.noreply.github.com>
2021-10-25 23:29:37 +0500

    Parsers: Add common parser for breadcrumb

---
+++ src/parsers/common/Breadcrumb.js
@@ -0,0 +1,37 @@
+import { removeInvisible } from "Utils/strings"
+
+class Breadcrumb {
+
+    constructor() {
+        /** @type {string} */
+        this.courseName = null;
+
+        /** @type {number} */
+        this.courseId = null;
+
+        /** @type {string} */
+        this.quizName = null;
+        
+        /** @type {number} */
+        this.quizId = null;
+
+
+        document.querySelectorAll(".breadcrumb a").forEach(a => {
+            const url = new URL(a.href);
+
+            switch (url.pathname) {
+                case "/course/view.php":
+                    this.courseName = removeInvisible(a.innerText);
+                    this.courseId = parseInt(url.searchParams.get("id"));
+                    break;
+
+                case "/mod/quiz/view.php":
+                    this.quizName = removeInvisible(a.innerText);
+                    this.quizId = parseInt(url.searchParams.get("id"));
+                    break;
+            }
+        });
+    }
+}
+
+export default new Breadcrumb();
