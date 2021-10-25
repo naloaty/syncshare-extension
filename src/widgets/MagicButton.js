a2845fbd6615e5a7cc8ab8273501ef25dca773d1
Naloaty <53543446+Naloaty@users.noreply.github.com>
2021-10-25 23:34:40 +0500

    Widgets: Add function to create magic button

---
+++ src/widgets/MagicButton.js
@@ -0,0 +1,7 @@
+const createMagicButton = () => {
+    const btn = document.createElement("span");
+    btn.className = "ss-btn icon fa fa-magic fa-fw";
+    return btn;
+}
+
+export { createMagicButton }
