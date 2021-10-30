74de9b24838d76c6b29f8face99c025bac2268f7
Naloaty <53543446+Naloaty@users.noreply.github.com>
2021-10-30 21:17:57 +0500

    Parsers: refactor 'match' question parser

--- src/parsers/quiz/Match.js
+++ src/parsers/quiz/Match.js
@@ -1,6 +1,7 @@
 import Question from "Parsers/quiz/Question"
 import * as Images from "Utils/images"
 import * as Sign from "Utils/signature"
+import * as Array from "Utils/arrays"
 import createMagicButton from "Widgets/MagicButton"
 import { removeInvisible } from "Utils/strings"
 import ssdeep from "Utils/ssdeep"
@@ -10,42 +11,42 @@ class Match extends Question {
     constructor(args) {
         super(args);
 
-        const table = this.container.querySelector("table.answer");
-        const stems = table.querySelectorAll("td.text");
+        const table   = this.container.querySelector("table.answer");
+        const stems   = table.querySelectorAll("td.text");
         const selects = table.querySelectorAll("td.control > select");
 
-        this.map = {};
-        this.optionMap = {}
+        this.labels  = {};
+        this.options = {};
 
-        for (const option of selects[0].childNodes) {
-            this.optionMap[option.innerText] = option.value;
-        }
+        Array.forEach(selects[0].childNodes, option => {
+            this.options[option.innerText] = option.value;
+        });
 
-        for (let i = 0; i < stems.length; i++) {
-            const stem = stems[i];
+        Array.forEach(stems, (stem, i) => {
             const select = selects[i];
 
             let meta = removeInvisible(stem.innerText);
 
-            for (const image of stem.querySelectorAll("img"))
-                meta += Images.serialize(image);
+            Array.forEach(stem.querySelectorAll("img"), image => {
+                meta += Images.serialize(image) + ";";
+            });
 
-            this.map[ssdeep.digest(meta)] = select;
-        }
+            this.labels[ssdeep.digest(meta)] = select;
+        });
     }
 
     createWidgetAnchor(anchor) {
-        let select = this.map[anchor];
+        let select = this.labels[anchor];
 
         // Try to find similar nodes in case 
         // the text of the question has changed
         if (!select) {
-            const similar = Sign.findSimilar(anchor, Object.keys(this.map));
+            const similar = Sign.findSimilar(anchor, Object.keys(this.labels));
 
             // If there are several similar values
             // then anchoring is not possible
             if (similar.length == 1)
-                select = this.map[similar[0]];
+                select = this.labels[similar[0]];
         }
 
         if (!select)
@@ -54,7 +55,7 @@ class Match extends Question {
         const button = createMagicButton();
         select.parentNode.appendChild(button);
 
-        const onClick = data => select.value = this.optionMap[data.text];
+        const onClick = data => select.value = this.options[data.text];
 
         return { onClick, button };
     }
