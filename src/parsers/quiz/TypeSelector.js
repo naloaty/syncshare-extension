6435c38c97dea1f8afd5c7e5cd555a0e2f090ef1
Naloaty <53543446+Naloaty@users.noreply.github.com>
2021-10-30 21:17:04 +0500

    Parsers: add type selector for quiz questions

---
+++ src/parsers/quiz/TypeSelector.js
@@ -0,0 +1,38 @@
+import logger from "Internal/log"
+import Match from "Parsers/quiz/Match"
+import Multianswer from "Parsers/quiz/Multianswer"
+import Multichoice from "Parsers/quiz/Multichoice"
+import Shortanswer from "Parsers/quiz/Shortanswer"
+
+class TypeSelector {
+
+    /**
+     * Selects appropriate class to correctly handle question.
+     * 
+     * @param {HTMLDivElement} container Question div container
+     */
+    select(container) {
+        switch (container.classList[1]) {
+            case "match": 
+                return new Match(container);
+
+            case "multianswer": 
+                return new Multianswer(container);
+
+            case "multichoice": 
+            case "truefalse": 
+                /* TODO: create seperate handler for truefalse question */
+                return new Multichoice(container);
+
+            case "shortanswer": 
+            case "numerical": 
+            case "calculated": 
+                return new Shortanswer(container);
+
+            default:
+                logger.warn("Unknown question type", container.classList[1]);
+        }
+    }
+}
+
+export default new TypeSelector();
