678a056602db85bd59e5727db526d19b07b6950a
Naloaty <53543446+Naloaty@users.noreply.github.com>
2021-11-02 17:58:51 +0500

    Service: Add quiz service

---
+++ src/service/quiz.js
@@ -0,0 +1,44 @@
+import logger from "Internal/log";
+import browser from "webextension-polyfill";
+
+class QuizService {
+    
+    init() {
+        browser.runtime.onMessage.addListener(data => {
+            if (data?.type !== "quiz-review-data")
+                return;
+
+            this.onReviewData(data.payload)
+        });
+
+        browser.runtime.onMessage.addListener(data => {
+            if (data?.type !== "quiz-attempt-data")
+                return;
+
+            this.onAttemptData(data.payload);
+        });
+
+        browser.runtime.onMessage.addListener((data, sender, sendResponse) => {
+            if (data?.type !== "solution-request")
+                return;
+
+            this.onSoultionRequest(data.payload, sendResponse);
+        });
+
+        logger.info("QuizService: initialized");
+    }
+
+    onReviewData(data) {
+        console.log("Review data received");
+    }
+
+    onAttemptData(data) {
+        console.log("Attempt data received", data);
+    }
+
+    onSoultionRequest(body, sendResponse) {
+        console.log("Solution request received", body);
+    }
+}
+
+export default QuizService;
