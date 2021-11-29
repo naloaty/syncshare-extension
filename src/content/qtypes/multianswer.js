861c4780caf5ee2b96b662f6c2c3c7965659bab0
Naloaty <53543446+Naloaty@users.noreply.github.com>
2021-11-29 19:02:16 +0500

    Multianswer: fix answers parsing (again 3)

--- src/content/qtypes/multianswer.js
+++ src/content/qtypes/multianswer.js
@@ -153,7 +153,7 @@ class MultiAnswerQ extends Question {
             if (subq.type === this.types.combo) {
                 const value = subq.select.value;
 
-                if (!Number.isNaN(parseInt(value)))
+                if (Number.isNaN(parseInt(value)))
                     continue;
 
                 const que = {
